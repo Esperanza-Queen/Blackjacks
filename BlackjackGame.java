86ff9606a5dfb6c606ad088e77b060d0dbcfef65
Esperanza-Queen <monajoya559@Gmail.com>
2017-10-02 13:59:01 -0700

    2.29.17

---
+++ BlackjackGame.java
@@ -0,0 +1,22 @@
+
+/**
+ * 2nd Class Project 
+ * 
+ * @author (Esperanza Joya) 
+ * @version (09.25.17)
+ */
+import java.util.Random ; 
+import java.util.Scanner;
+public class BlackjackGame
+{
+    //Instance Variables 
+    int Card1; 
+    int Card2;
+    int Card3;
+    int Card4; 
+    
+    
+    // Constructor (Zero Parameter)
+    public static void BlackjackGame() { 
+     }
+}
---
+++ Card.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Hand here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.lang.Math ;
+public class Card
+{
+    int numOfCards;
+    int total;
+    //Pubic draw Public getRandNum , Tag each card (5) if statements for all cards , use int tags to target the totals parentheses and + to determine the total 
+    
+    public Card(){
+        //Dealt two cards
+    }
+
+    public void cards () {
+        int g = 0-11 ; 
+        while (0 < g ){
+           }
+    }
+    
+}
+
+
