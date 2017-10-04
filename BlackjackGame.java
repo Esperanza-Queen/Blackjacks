c6c34029adf623c9adc4588ed60884c6dfe1789f
Esperanza_Queen <monajoya559@Gmail.com>
2017-10-04 14:27:49 -0700

    Resyncing

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -1,3 +1,5 @@
+package Blackjack;
+
 
 /**
  * 2nd Class Project 
@@ -10,13 +12,36 @@ import java.util.Scanner;
 public class BlackjackGame
 {
     //Instance Variables 
-    int Card1; 
-    int Card2;
-    int Card3;
-    int Card4; 
+    int Card1p; 
+    int Card2p;
+    int Card3d;
+    int Card4d; 
+    int CardN;
     
-    
+    int playertotal; Card1p + Card2 = total
+   
     // Constructor (Zero Parameter)
     public static void BlackjackGame() { 
+        Card1p; 
+        Card2p;
+        Card3d;
+        Card4d;
+        Cardn; 
+        
      }
+    
+    public getRandNum (){
+       if ( playertotal = 21){
+              system.out.println (" You win")
+            }
+         else if ( playertotal < 21 ) {
+              system.out.println (" You Lost ")
+            }
+            else if ( playertotal > 21) {
+              system.out.println (" Draw ? ")
+        }
+          else if ( playertotal > 21 ) {
+                system.out.println (" Stay ")
+            }        
+    }
 }
